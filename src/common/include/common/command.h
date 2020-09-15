bc88cc43ac941da6babeca880f5c4332a90094a4
Jeremy Soller <jeremy@system76.com>
2020-09-15 13:19:27 -0600

    Commands for reading and setting keyboard map

--- src/common/include/common/command.h
+++ src/common/include/common/command.h
@@ -20,6 +20,10 @@ enum Command {
     CMD_FAN_GET = 7,
     // Set fan speeds
     CMD_FAN_SET = 8,
+    // Get keyboard map index
+    CMD_KEYMAP_GET = 9,
+    // Set keyboard map index
+    CMD_KEYMAP_SET = 10,
     //TODO
 };
 
